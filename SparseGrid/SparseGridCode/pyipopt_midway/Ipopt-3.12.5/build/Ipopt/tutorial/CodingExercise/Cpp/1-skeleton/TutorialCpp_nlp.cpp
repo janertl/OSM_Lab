523bba547cfd186f45285a0a70c5b5f724ce7a8d
rccguest 0039 <rccguest0039@midway-login2.rcc.local>
2017-07-24 15:34:23 -0500

    installed SG

---
+++ SparseGrid/SparseGridCode/pyipopt_midway/Ipopt-3.12.5/build/Ipopt/tutorial/CodingExercise/Cpp/1-skeleton/TutorialCpp_nlp.cpp
@@ -0,0 +1,1 @@
+/home/rccguest0039/OSM_Lab/SparseGrid/SparseGridCode/pyipopt_midway/Ipopt-3.12.5/Ipopt/tutorial/CodingExercise/Cpp/1-skeleton/TutorialCpp_nlp.cpp
---
+++ SparseGrid/SparseGridCode/pyipopt_midway/Ipopt-3.12.5/build/Ipopt/tutorial/CodingExercise/Matlab/1-skeleton/TutorialMatlab.m
@@ -0,0 +1,1 @@
+/home/rccguest0039/OSM_Lab/SparseGrid/SparseGridCode/pyipopt_midway/Ipopt-3.12.5/Ipopt/tutorial/CodingExercise/Matlab/1-skeleton/TutorialMatlab.m
